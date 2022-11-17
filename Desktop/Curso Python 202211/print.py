465781b270009b7a8f17d2eefd78d94585315b17
Keren Hapuc Hernandez <kerenhapuchernandez@MacBook-Pro-de-Keren.local>
2022-11-16 21:02:25 -0600

    Mis practicas de Python

---
+++ Desktop/Curso Python 202211/print.py
@@ -0,0 +1,22 @@
+# Ejemplos de la función print()
+
+# print ("Hola mundo")
+# print ("Hola mundo","otra vez")
+# print ("Son las", 9, "de la mañana")
+# print ("El resultado de 3 * 4 es: ", 3*4)
+
+#Ejemplos de cadenas formateadas
+#print ('El número 15 en sistema decimal es %d, en sistema octal es %o, en sistema hexadecimal es %x' %(15,15,15))
+
+# pi = 3.141592
+# r = 5
+# print(f'el radio de un círculo es {r} y el área de ese círculo es {pi * r ** 2 : .2f}')
+
+#Impresión de carácteres especiales
+#print ('La letra beta es: \n\t \u03B2')# n es para salto de línea y t para tabulación, símbolo con código ASCII
+
+#Cáracteres de escape
+print ('Hola mundo', end= ' ')
+print ('otra vez', end='\t')
+print ('y otra vez')
+
